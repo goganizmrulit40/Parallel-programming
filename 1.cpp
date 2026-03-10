426c82564676cb895965588c4d49f65acffca8c3
Maximord <m4ximyarosl@yandex.ru>
2026-03-10 20:26:42 +0400

    Заполнение матрицы случайными числами и использование написанных функций в main
    Напишем функцию заполнения квадратной матрицы случайными числами. Затем в main зададим массив с размерами матриц для последующих экспериментов. Проходя по всем размерам, создаём матрицы заданного размера и заполняем их случайными числами

--- 1.cpp
+++ 1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // Функция для умножения матриц
 vector<vector<double>> multiply(const vector<vector<double>>& A,
-    const vector<vector<double>>& B){
+    const vector<vector<double>>& B) {
     int n = A.size();
     vector<vector<double>> C(n, vector<double>(n, 0.0));
 
@@ -19,6 +19,29 @@ vector<vector<double>> multiply(const vector<vector<double>>& A,
     return C;
 }
 
-int main(){
+// Функция для заполнения матрицы случайными числами
+void fillRandom(vector<vector<double>>& matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = rand() % 100 / 10.0;
+        }
+    }
+}
+
+
+int main() {
+    int sizes[] = { 200, 400, 800, 1200, 1600, 2000 };
+    int num_sizes = 6;
     
+    for (int s = 0; s < num_sizes; s++) {
+        int n = sizes[s];
+        cout << "Размер матрицы: " << n << endl;
+    
+        vector<vector<double>> A(n, vector<double>(n));
+        vector<vector<double>> B(n, vector<double>(n));
+    
+        fillRandom(A);
+        fillRandom(B);
+    }
 }
